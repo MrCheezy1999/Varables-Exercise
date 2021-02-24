1e6eac18f0ec5ac52d45bd7df9090253b39ab3d8
MrCheezy1999 <ebb1999@live.com>
2021-02-24 01:53:22 -0600

    Done

---
+++ variablesExercise.js
@@ -0,0 +1,19 @@
+// const
+let num = 20;
+console.log(num);
+num = 15;
+console.log(num);
+num += 7;
+console.log(num);
+num--;
+console.log(num);
+num = "This is not a number";
+console.log(num);
+let blank;
+console.log(blank);
+blank = null;
+console.log(blank);
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+console.log(quarter, dime, nickel, penny);
+let letters = "qwertyuiop";
+console.log(letters[letters.length - 1]);
